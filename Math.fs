def020444c503f4addf686917ce45e9eb7074cbd
JeehoonLee <mastalji@gmail.com>
2024-03-11 14:06:16 +0900

    at1

--- Math.fs
+++ Math.fs
@@ -5,5 +5,10 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
+
 let collatz n =
-  0
+    let rec times k m = 
+        if m=1 then k
+        elif (m%2=0) then times (k+1) (m/2)
+        else times (k+1) (3*m+1)
+    times 0 n
